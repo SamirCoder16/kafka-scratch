2b74d96a502f892203aff41ea1612e4b1f6e78ab
SamirCoder16 <samirparvez48@gmail.com>
2026-02-11 19:21:01 +0530

    comment added

--- consumer.js
+++ consumer.js
@@ -1,6 +1,6 @@
 const { kafka } = require('./client');
 
-const group = process.argv[2]
+const group = process.argv[2]; /// This process.argv[2] is used to pass the group name when running the consumer, for example: node consumer.js north
 
 async function init() {
     const consumer = kafka.consumer({ groupId : group });
@@ -8,7 +8,7 @@ async function init() {
     await consumer.connect();
     console.log("Consumer Connected Successfully");
 
-    await consumer.subscribe({ topics: ["rider-update"] , fromBeginning: true });
+    await consumer.subscribe({ topics: ["rider-update"] , fromBeginning: true }); // this fromBeginning: true is used to consume messages from the beginning of the topic, if you want to consume only new messages, you can set it to false
 
     await consumer.run({
         eachMessage: async ({ topic, partition, message , heartbeat, pause}) => {
